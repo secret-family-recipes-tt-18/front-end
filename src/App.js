94d3a840b52ff2521bd8d3285b24b3f61e096449
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-01-04 11:19:02 -0500

    Creating Login form and trying to connect with API

--- src/App.js
+++ src/App.js
@@ -10,12 +10,17 @@ import { RecipesContext } from "./contexts/RecipesContext";
 
 //components
 import Landing from './components/Landing';
+import Login from './forms/Login';
 
 //utils
 import PrivateRoute from "./components/PrivateRoute";
 import axiosWithAuth from "./utils/axiosWithAuth";
 
 function App() {
+  //hooks
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
   return (
     <RecipesContext.Provider
       value ={[]}
@@ -24,6 +29,7 @@ function App() {
         <Router>
           <Switch>
             <Route exact path='/' component={Landing} />
+            <Route path ='/login' component={Login} />
           </Switch>
         </Router>
       </div>
---
+++ src/forms/Login.js
@@ -0,0 +1,60 @@
+import React, { useState } from  'react';
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+
+import axiosWithAuth from './../utils/axiosWithAuth';
+import { BACKEND_URL } from '../utils/util';
+
+
+const initialUserData = {
+    username: "",
+    password: ""
+  };
+
+const Login = (props) => {
+
+    const { push } = useHistory();
+
+    const [userData, setUserData] = useState(initialUserData);
+
+
+    const changeHandle = (event) => {
+        const { name, value } = event.target;
+        setUserData({ ...userData, [name]: value });
+    }
+
+    const submitHandle = (event) => {
+        event.preventDefault();
+        axios
+        .post(`${BACKEND_URL}/api/auth/login/login`)
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.log("Error:",err);
+        });
+    }
+
+    return ( <div className='login-body'>
+    <h1>Log In</h1>
+    <form onSubmit={submitHandle}>
+      <input
+        name='username'
+        type='text'
+        placeholder='Username'
+        value={userData.username}
+        onChange={changeHandle}
+      />
+      <input
+        name='password'
+        type='password'
+        placeholder='Password...'
+        value={userData.password}
+        onChange={changeHandle}
+      />
+      <button>Submit...</button>
+    </form>
+  </div>)
+}
+
+export default Login;
