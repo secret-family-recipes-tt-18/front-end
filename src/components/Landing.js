31fb7286307813a690337cf1d9291af6046c881b
Johnny Murillo <jossters@gmail.com>
2021-01-05 15:24:45 -0800

    landing page style changes

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,28 +1,40 @@
 import React, { useContext } from "react";
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
 import bg from "../images/bg.jpg";
 
-import { RecipesContext } from '../contexts/RecipesContext';
+import { RecipesContext } from "../contexts/RecipesContext";
 
 const Landing = () => {
-    
-    const { push } = useHistory();
+  const { push } = useHistory();
 
-    //hooks
-    const { loggedInHook } = useContext(RecipesContext);
+  //hooks
+  const { loggedInHook } = useContext(RecipesContext);
 
-    return (<div className="landing">
-        <div className="limage">
-            <img src={bg} alt="background"/>
-        </div>
-        {loggedInHook.isLoggedIn ? null : 
-            <div className="lcontain">
-                <button onClick={()=>{push('/login')}}>Login</button>
-                <button onClick={()=>{push('/signup')}}>Sign Up</button>
-            </div>
-        }
-    </div>)
-}
+  return (
+    <div className="landing"style={{ backgroundImage: `url(${bg})` }}>
+        <h1 className="ltitle">Secret Family Recipes</h1>
+        {loggedInHook.isLoggedIn ? null : (
+          <div className="lcontain">
+            <button
+              onClick={() => {
+                push("/login");
+              }}
+            >
+              Login
+            </button>
+            <button
+              onClick={() => {
+                push("/signup");
+              }}
+            >
+              Sign Up
+            </button>
+          </div>
+        )}
+      
+    </div>
+  );
+};
 
 export default Landing;
