7965de7fe66449d7423fca6847ca7fecf305ab6b
Johnny Murillo <jossters@gmail.com>
2021-01-06 09:10:36 -0800

    started on yup

--- src/forms/Signup.js
+++ src/forms/Signup.js
@@ -1,67 +1,76 @@
-import React, { useContext, useState } from  "react";
+import React, { useContext, useState } from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
 
-import { RecipesContext } from '../contexts/RecipesContext';
+import { RecipesContext } from "../contexts/RecipesContext";
 
-import { BACKEND_URL } from '../utils/util';
+import { BACKEND_URL } from "../utils/util";
 
 const initialFormValues = {
-    username: "",
-    password: ""
-  };
-
+  username: "",
+  password: "",
+};
 
 const Signup = (props) => {
+  const { push } = useHistory();
 
-    const { push } = useHistory(); 
+  //hooks
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const { pageLoadingHook } = useContext(RecipesContext);
 
-    //hooks
-    const [formValues, setFormValues] = useState(initialFormValues);
-    const { pageLoadingHook } = useContext(RecipesContext);
+  const changeHandle = (event) => {
+    const { name, value } = event.target;
 
-    const changeHandle = (event) => {
-        const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
 
-        setFormValues({ ...formValues, [name]: value });
-    }
+  const submitHandle = (event) => {
+    event.preventDefault();
+    pageLoadingHook.func(true);
+    axios
+      .post(`${BACKEND_URL}/api/auth/register`, formValues)
+      .then((res) => {
+        //console.log(res);
+        pageLoadingHook.func(false);
+        push("/login");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const submitHandle = (event) => {
-        event.preventDefault();
-        pageLoadingHook.func(true);
-        axios
-        .post(`${BACKEND_URL}/api/auth/register`,formValues)
-        .then(res => {
-            //console.log(res);
-            pageLoadingHook.func(false);
-            push('/login');
-        })
-        .catch(err => {
-            console.log(err);
-        });
-    }
+  return (
+    <div className="signup-body">
+      <div>
+        <h1>Sign Up</h1>
+        <form onSubmit={submitHandle}>
+          <div className="userin">
+            <input
+              name="username"
+              type="text"
+              placeholder="username"
+              value={formValues.username}
+              onChange={changeHandle}
+            />
+          </div>
 
-    return(<div className='signup-body'>
-    <h1>Sign Up</h1>
-    <form onSubmit={submitHandle}>
-      <input
-        name='username'
-        type='text'
-        placeholder='username'
-        value={formValues.username}
-        onChange={changeHandle}
-      />
-      <input
-        name='password'
-        type='password'
-        placeholder='password'
-        value={formValues.password}
-        onChange={changeHandle}
-      />
-      <button>Submit</button>
-      {pageLoadingHook.value ? <div>Loading</div> : null}
-    </form>
-  </div>)
-}
+          <div>
+            <input
+              name="password"
+              type="password"
+              placeholder="password"
+              value={formValues.password}
+              onChange={changeHandle}
+            />
+          </div>
+
+          <button className="submit-b">Sign up</button>
+
+          {pageLoadingHook.value ? <div>Loading</div> : null}
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Signup;
