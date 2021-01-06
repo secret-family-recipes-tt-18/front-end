f0f5f82b1b1cf291aea9ddad73a124f15f03f041
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-01-06 10:38:42 -0500

    add some categories

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,6 +1,6 @@
 export const BACKEND_URL = "https://tgif-secret-family-recipes-api.herokuapp.com";
 
-export const CUISINE_CATEGORIES = ["soup"];
+export const CUISINE_CATEGORIES = ["soup", "salad", "pasta", "rice", "sandwich", "wrap"];
 
 export const DETAIL_INITIAL_OBJ = {
     ingredients: [""],
